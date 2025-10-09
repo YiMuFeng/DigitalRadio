8e51f5e08971b7224249aa01e3d6fa4eb2639d34
MF.Y <1428483061@qq.com>
2025-10-09 21:57:28 +0800

    初始版本 完善版权声明和版本号管理

--- DigitalModulation.py
+++ DigitalModulation.py
@@ -1,11 +1,23 @@
-
+# Copyright (c) 2025 Y.MF. All rights reserved.
+#
+# 本代码及相关文档受著作权法保护，未经授权，禁止任何形式的复制、分发、修改或商业使用。
+# 如需使用或修改本代码，请联系版权所有者获得书面许可（联系方式：1428483061@qq.com）。
+#
+# 免责声明：本代码按"原样"提供，不提供任何明示或暗示的担保，包括但不限于对适销性、特定用途适用性的担保。
+# 在任何情况下，版权所有者不对因使用本代码或本代码的衍生作品而导致的任何直接或间接损失承担责任。
+#
+# 项目名称：DigitalModulation.py
+# 项目仓库：https://github.com/YiMuFeng/DigitalRadio.git
+# 创建时间：2025/10/9 22:36
+# 版权所有者：Y.MF
+# 联系方式：1428483061@qq.com
+# 许可协议：Apache License 2.0
 
 """
 数字调制与解调模块
 提供各种数字调制解调方法
 包括数字变频和数字滤波
 """
-
 __version__ = "0.0.0.0"
 
 import numpy as np
@@ -21,7 +33,8 @@ class DigitalModulation:
     def __init__(self):
         pass
 
-    def am_modulate(self,signal:np.ndarray, carrier_freq:int, sample_rate:int,
+    @staticmethod
+    def am_modulate(signal:np.ndarray, carrier_freq:int, sample_rate:int,
                     modulation_index:float=0.5,normalization:bool=True):
         """
         调幅（AM）调制
@@ -47,7 +60,8 @@ class DigitalModulation:
 
         return modulated
 
-    def fm_modulate(self,signal:np.ndarray, carrier_freq:int, sample_rate:int,
+    @staticmethod
+    def fm_modulate(signal:np.ndarray, carrier_freq:int, sample_rate:int,
                     modulation_index:float=5.0,max_baseband_freq=20000,normalization:bool=True):
         """
         调频（FM）调制
@@ -78,7 +92,8 @@ class DigitalModulation:
 
         return modulated
 
-    def ssb_modulate(self,signal:np.ndarray, carrier_freq:int, sample_rate:int,
+    @staticmethod
+    def ssb_modulate(signal:np.ndarray, carrier_freq:int, sample_rate:int,
                      sideband='upper',normalization:bool=True):
         """
         单边带（SSB）调制
@@ -142,3 +157,6 @@ class DigitalModulation:
         # plt.pcolormesh(t, f, np.abs(Zxx), shading='gouraud')  # STFT时频谱（快速）
         # plt.show()
         return f,t,Zxx
+
+if __name__ == '__main__':
+    pass
--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-# 这是一个示例 Python 脚本。
+# Copyright (c) 2025 Y.MF. All rights reserved.
+#
+# 本代码及相关文档受著作权法保护，未经授权，禁止任何形式的复制、分发、修改或商业使用。
+# 如需使用或修改本代码，请联系版权所有者获得书面许可（联系方式：1428483061@qq.com）。
+#
+# 免责声明：本代码按"原样"提供，不提供任何明示或暗示的担保，包括但不限于对适销性、特定用途适用性的担保。
+# 在任何情况下，版权所有者不对因使用本代码或本代码的衍生作品而导致的任何直接或间接损失承担责任。
+#
+# 项目名称：main.py
+# 项目仓库：https://github.com/YiMuFeng/DigitalRadio.git
+# 创建时间：2025/10/9 22:36
+# 版权所有者：Y.MF
+# 联系方式：1428483061@qq.com
+# 许可协议：Apache License 2.0
 
-# 按 F5 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+"""
+"""
+__version__ = "0.0.0.0"
 
+import DigitalModulation
+import UI
 
 def print_hi(name):
     # 在下面的代码行中使用断点来调试脚本。
@@ -13,4 +29,3 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
